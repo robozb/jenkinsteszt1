b3f4f6221ff13bea6685c045e018281ee66bc084
RBeci <rbelatamas@gmail.com>
2021-12-04 23:34:22 +0100

    elso

---
+++ src/main/java/com/example/demo/RestTeszt1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Beci
+ */
+@RestController
+@RequestMapping("/rest")
+public class RestTeszt1 {
+    @GetMapping
+    public String get() {
+        return "ok";
+    }
+}
