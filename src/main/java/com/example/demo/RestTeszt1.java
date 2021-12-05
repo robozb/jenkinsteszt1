db9b0dcf6596bf637808b58e5a0f477c83c4aa11
RBeci <rbelatamas@gmail.com>
2021-12-05 02:44:15 +0100

    automata folyamat a jenkinssel

--- src/main/java/com/example/demo/RestTeszt1.java
+++ src/main/java/com/example/demo/RestTeszt1.java
@@ -18,6 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestTeszt1 {
     @GetMapping
     public String get() {
-        return "ok-hello5";
+        return "ok-automata folyamat a Jenkinsszel";
     }
 }
